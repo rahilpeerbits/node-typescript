c166139c60d2ccdddd2f5301d08072c1dde0f8d1
Rahil <rahil@peerbits.com>
2021-05-26 18:15:10 +0530

    remove Jasmine

--- src/v1/auth/login.spec.ts
+++ src/v1/auth/login.spec.ts
@@ -1,5 +1,12 @@
-describe("profile", () => {
-  it("get", () => {
-    expect(7).toBe(7);
-  });
-});
+// import request from 'supertest-as-promised';
+// import server from "../../app";
+// describe("POST /login", () => {
+//   it("login user", async () => {
+//     const body = {
+//         email: 'rahil@peerbits.com',
+//         password: '123456'
+//     }
+//     const res = request(server).post('/v1/user/login').send(body);
+//     expect(res).toBeTrue();
+//   });
+// });
